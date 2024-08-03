ece4ca7c5369246337fd5a2fcd89f31c27547fbb
omodot97 <109751174+omodot97@users.noreply.github.com>
2024-08-03 11:13:06 +0100

    Packet_Analyzer.py

--- bxbx.py
+++ bxbx.py
@@ -1,26 +1,3 @@
-#Basic packet sniffer
-# from scapy.all import sniff
-
-#Define a callback function that will be called for each captured packet
-#def packet_callback(packet):
-# print(packet.summary())
-
-#start sniffing on network interface
-#sniff(prn=packet_callback, count=50)
-
-
-#Analyzing the packets
-
-#from scapy.all import sniff, IP
-
-#def packet_callback(packet):
-    #if packet.haslayer(IP):
-        #ip_layer = packet.getlayer(IP)
-       # print(f"source: {ip_layer.src}, Destination: {ip_layer.dst}, protocol:{ip_layer.proto}")
-
-#sniff(prn=packet_callback, count=10)
-
-
 #Saving and Reading Packets
 from scapy.all import sniff, IP, wrpcap, rdpcap
 
